4a0fc87eb8f398f0160c725c2d1d1ee4d52361f3
wangyuhang0469 <835779808@qq.com>
2018-03-14 15:46:58 +0800

    上传

---
+++ app/src/main/java/com/example/administrator/kenya/ui/myself/ForgetPasswordActivity.java
@@ -0,0 +1,32 @@
+package com.example.administrator.kenya.ui.myself;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.administrator.kenya.R;
+import com.example.administrator.kenya.base.BaseActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ForgetPasswordActivity extends BaseActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+
+
+    }
+
+
+
+
+}
